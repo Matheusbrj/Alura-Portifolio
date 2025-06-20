3c38742e339e91f467ce912526ac903ccde39939
MatheusBrj <00001118601658SP@al.educacao.sp.gov.br>
2025-06-20 17:07:35 -0300

    Complete.

---
+++ Portifolio-Alura/src/Engine.js
@@ -0,0 +1,3 @@
+document.getElementById('about-photo').onclick = function () {
+    alert('Esse é o pessoal da minha igreja! amo muito todos eles. Essa foto foi tirada depois de um evangelismo, em 23 de Maio, 2025');
+};
